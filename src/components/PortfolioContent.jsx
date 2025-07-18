76f2e7c9a333d9c9b4ab1ed15d25588bc5be3bee
jyg <qwer2435@naver.com>
2025-07-18 13:29:10 +0900

    - 박스 상단으로 넘치는 오류 수정 - 이미지 크기 및 스타일 수정

--- src/components/PortfolioContent.jsx
+++ src/components/PortfolioContent.jsx
@@ -26,16 +26,28 @@ function PortfolioContent({ onScroll }) {
 
   return (
     <div className="portfolio-overlay-content flex-col-center" ref={contentRef}>
-      <div className="portfolio-main-content">
+      <div className="portfolio-main-content flex-between introduce">
+        <div className='introduce-text'>
+          <p></p>
+          <p></p>
+          <p></p>
+        </div>
+        <img src="/myprofile-clean.png" alt="myProfile" />
       </div>
 
-      <div className="portfolio-main-content">
+      <div className="portfolio-main-content ability">
+        언어: HTML, CSS, JavaScript, TypeScript
+        프레임워크/라이브러리: React, Jquery
+        스타일링: Bootstap, Styled Components, Sass, responsive
+        버전 관리: GitHub, GitLab
+        디자인 툴 : Figma, Zeplin
       </div>
 
-      <div className="portfolio-main-content">
+      <div className="portfolio-main-content project">
+
       </div>
       
-      <div className="portfolio-footer">
+      <div className="portfolio-main-content">
         <div>qwer2435@naver.com</div>
         <div>01040199816</div>
       </div>
